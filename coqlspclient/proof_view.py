6fb4634ba8937b197ea4fb2b094fae9d4eee419e
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2023-07-27 16:08:11 +0200

    Small fix

--- coqlspclient/proof_view.py
+++ coqlspclient/proof_view.py
@@ -205,7 +205,7 @@ class ProofView(object):
         proof_verdicts = []
 
         logger.info(f"Start processing various proofs.")
-        with alive_bar(len(self.ast)) as bar:
+        with alive_bar(len(proofs)) as bar:
             for proof in proofs:
                 new_text = aux_file_text + proof
                 document_version += 1
