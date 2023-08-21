ef7c3906b9cbb65f794265fc7f74c1356e41eaa8
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2023-08-21 14:52:45 +0200

    Update imports

--- pylspclient/__init__.py
+++ pylspclient/__init__.py
@@ -2,7 +2,7 @@
 
 __all__ = []
 
-from pylspclient.json_rpc_endpoint import JsonRpcEndpoint
-from pylspclient.lsp_client import LspClient
-from pylspclient.lsp_endpoint import LspEndpoint
-from pylspclient import lsp_structs
+from .json_rpc_endpoint import JsonRpcEndpoint
+from .lsp_client import LspClient
+from .lsp_endpoint import LspEndpoint
+from . import lsp_structs
